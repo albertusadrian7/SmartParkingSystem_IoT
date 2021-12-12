07238da24533a60476219b1aba353f2bc1ab7603
albertusadrian7 <albertsadrian7@gmail.com>
2021-12-12 19:20:49 +0700

    Inisialisasi realtime database: Read data

--- app/src/main/java/id/ac/ukdw/smartparking/DashboardPengurus.kt
+++ app/src/main/java/id/ac/ukdw/smartparking/DashboardPengurus.kt
@@ -1,11 +1,46 @@
 package id.ac.ukdw.smartparking
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import id.ac.ukdw.smartparking.databinding.ActivityDashboardPengurusBinding
 
 class DashboardPengurus : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+    private lateinit var binding: ActivityDashboardPengurusBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_pengurus)
+        binding = ActivityDashboardPengurusBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Write a message to the database
+        database = Firebase.database.reference
+
+        val databaseListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get data dari firebase dan update value ke tampilan UI
+                /*
+                val parkiran = dataSnapshot.child("parkiran").getValue<Double>().toString()
+                val pemasukan = dataSnapshot.child("pemasukan").getValue<Double>().toString()
+                 */
+                val pengunjung = dataSnapshot.child("pengunjung").getValue<Double>().toString()
+                binding.tvFirebase.text = "Pengunjung: $pengunjung"
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(ContentValues.TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        database.addValueEventListener(databaseListener)
     }
 }
--- app/src/main/java/id/ac/ukdw/smartparking/MainActivity.kt
+++ app/src/main/java/id/ac/ukdw/smartparking/MainActivity.kt
@@ -1,11 +1,25 @@
 package id.ac.ukdw.smartparking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.ac.ukdw.smartparking.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        navigateToDashboardPengurus()
     }
+
+    private fun navigateToDashboardPengurus(){
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, DashboardPengurus::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
